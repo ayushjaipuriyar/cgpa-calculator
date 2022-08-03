f15669e1348926eef5f2b48372440169df59d2ea
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2022-08-04 00:29:45 +0530

    Service Worker Changes

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -16,17 +16,17 @@ self.addEventListener('install', (event) => {
 	);
 });
 self.addEventListener('fetch', (event) => {
-	// event.respondWith(
-	// 	caches.match(event.request).then(() => {
-	// 		return fetch(event.request).catch(() => caches.match('offline.html'));
-	// 	}),
 	event.respondWith(
-		caches.match(event.request).then((response) => {
-			if (response) {
-				return response;
-			}
-			return fetch(event.request);
+		caches.match(event.request).then(() => {
+			return fetch(event.request).catch(() => caches.match('offline.html'));
 		}),
+		// event.respondWith(
+		// caches.match(event.request).then((response) => {
+		// if (response) {
+		// return response;
+		// }
+		// return fetch(event.request);
+		// }),
 	);
 });
 self.addEventListener('activate', (event) => {
